eebe28666729d323334f261525cd2e33934165ca
jannatuladol <jannatuladol135@gmail.com>
2022-04-19 01:52:19 +0600

    adol

--- Victory Day/main.cpp
+++ Victory Day/main.cpp
@@ -369,6 +369,797 @@ void myDisplay(void)
 //clouds here
     danimateCloud();
 
+     glColor3f(0.7, 0.3, 0.2) ;
+    glBegin(GL_QUADS);
+    glVertex2i (0, 30);
+    glVertex2i (0, 15);
+    glVertex2i (400, 15);
+    glVertex2i (400, 30);
+    glEnd();
+
+    //ground 2nd
+
+    glColor3f(0.5, 0.3, 0.2) ;
+    glBegin(GL_QUADS);
+    glVertex2i (0, 15);
+    glVertex2i (0, -50);
+    glVertex2i (400, -50);
+    glVertex2i (400, 15);
+    glEnd();
+
+
+    //ground 3rd
+    glColor3f(0.4, 0.3, 0.2) ;
+    glBegin(GL_QUADS);
+    glVertex2i (0, -50);
+    glVertex2i (0, -70);
+    glVertex2i (400, -70);
+    glVertex2i (400, -50);
+    glEnd();
+
+
+
+    //trin 1 main
+    glColor3ub(29, 130, 32) ;
+    glBegin(GL_QUADS);
+    glVertex2i (0, 130);
+    glVertex2i (0, 30);
+    glVertex2i (400, 30);
+    glVertex2i (400, 130);
+    glEnd();
+
+
+
+
+
+
+    // Hill
+
+
+    glPushMatrix();
+    //sun in the back
+
+
+    glTranslatef(30, 120, 0);
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=55;
+        float x = r * cos(A);
+        float y = r * sin(A);
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(80, 140, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(100, 140, 0);
+
+
+
+    glPushMatrix();
+
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(120, 140, 0);
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(180, 120, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=55;
+        float x = r * cos(A);
+        float y = r * sin(A);
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(270, 110, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=55;
+        float x = r * cos(A);
+        float y = r * sin(A);
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(270, 145, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(290, 140, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y);
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(310, 140, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(360, 120, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(29, 130, 32);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=55;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    //Tree 1
+    glPushMatrix();
+    glTranslatef(-28, 23, 0);
+
+
+    glPushMatrix();
+    glTranslatef(50, 170, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(60, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(40, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glColor3ub(120, 32, 12);
+    glBegin(GL_QUADS);
+    glVertex2i(48, 130);
+    glVertex2i(47, 40);
+    glVertex2i(52, 40);
+    glVertex2i(51, 130);
+    glEnd();
+
+
+    glPopMatrix();
+
+
+//Tree 2
+
+
+    glPushMatrix();
+    glTranslatef(320, 43, 0);
+
+
+    glPushMatrix();
+    glTranslatef(50, 170, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(60, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(40, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glColor3ub(120, 32, 12);
+    glBegin(GL_QUADS);
+    glVertex2i(48, 130);
+    glVertex2i(47, 40);
+    glVertex2i(52, 40);
+    glVertex2i(51, 130);
+    glEnd();
+
+
+    glPopMatrix();
+
+//Tree 3
+    glPushMatrix();
+    glTranslatef(0, 37, 0);
+
+
+    glPushMatrix();
+    glTranslatef(50, 170, 0);
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(60, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(40, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glColor3ub(120, 32, 12);
+    glBegin(GL_QUADS);
+    glVertex2i(48, 130);
+    glVertex2i(47, 67);
+    glVertex2i(52, 67);
+    glVertex2i(51, 130);
+    glEnd();
+
+    glPopMatrix();
+
+
+//Tree 4
+    glPushMatrix();
+    glTranslatef(35, 13, 0);
+
+
+    glPushMatrix();
+    glTranslatef(50, 170, 0);
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(60, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(40, 150, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glColor3ub(120, 32, 12);
+    glBegin(GL_QUADS);
+    glVertex2i(48, 130);
+    glVertex2i(47, 40);
+    glVertex2i(52, 40);
+    glVertex2i(51, 130);
+    glEnd();
+
+
+    glPopMatrix();
+
+
+// Tree 5
+    glPushMatrix();
+    glTranslatef(290, 4, 0);
+
+    glPushMatrix();
+    glTranslatef(50, 170, 0);
+
+
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*2;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslatef(60, 150, 0);
+
+
+    glPushMatrix();
+
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(40, 150, 0);
+
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+    glColor3ub(12, 120, 41);
+
+
+    for(int i=0; i<200; i++)
+    {
+        float pi=3.1416;
+        float A=(i*2*pi)/200;
+        float r=15;
+        float x = r * cos(A);
+        float y = r * sin(A)*1.5;
+        glVertex2f(x,y );
+    }
+    glEnd();
+
+
+    glPopMatrix();
+    glPopMatrix();
+
+
+    glColor3ub(120, 32, 12);
+    glBegin(GL_QUADS);
+    glVertex2i(48, 130);
+    glVertex2i(47, 70);
+    glVertex2i(52, 70);
+    glVertex2i(51, 130);
+    glEnd();
+
+
+    glPopMatrix();
+
     glPopMatrix();
     glFlush();
 }
